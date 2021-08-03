6b88a6db24b5e70dbda5118d79ead81b7767c031
HasanRifat <rifat562hasan@gmail.com>
2021-08-03 16:21:07 -0700

    add php

--- contactForm.php
+++ contactForm.php
@@ -7,9 +7,9 @@ if (isset($_POST['submit'])) {
     $message = $_POST['message'];
 
     $mailTo = "rifat.hasan@student.csulb.edu";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $headers = "From: " . $mailFrom;
+    $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend")
+    header("Location: index.php?mailsend");
 }
